223287b5982a43409fe9a8088c0f8c63ebb0f31b
jeeinn <thinkwei2012@gmail.com>
2022-09-20 17:17:48 +0800

    U: vditor toolbar & hot key

--- src/components/Vditor.vue
+++ src/components/Vditor.vue
@@ -7,6 +7,7 @@ import Vditor from 'vditor'
 import 'vditor/dist/index.css'
 import {ElMessageBox} from "element-plus"
 import {ElNotification} from 'element-plus';
+import svgIcons from '../config/vditor-toolbar-svg.js'
 // 导入系统组件
 import {dialog} from "@tauri-apps/api"
 import {window} from "@tauri-apps/api"
@@ -26,7 +27,7 @@ export default {
   mounted() {
     let self = this
     this.vditor = new Vditor('vditorEle', {
-      // mode: 'sv', 默认为即时渲染模式
+      // mode: 'sv', 默认为即时渲染ir模式
       // cdn: '..',
       lang: self.lang,
       cache: {
@@ -44,63 +45,146 @@ export default {
       },
       toolbar: [
         {
-          name: "openSave",
+          name: "openOrSave",
           tip: "打开/保存",
-          icon: '<svg t="1597727407471" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1167" width="32" height="32"><path d="M376.832 474.112H130.048c-33.792 0-61.44-27.648-61.44-61.44V165.888c0-33.792 27.648-61.44 61.44-61.44h247.296c33.792 0 61.44 27.648 61.44 61.44v247.296c-0.512 33.792-27.648 60.928-61.952 60.928zM130.048 165.888v247.296h247.296V165.888H130.048zM739.84 525.312c-16.384 0-31.744-6.144-43.52-17.92L521.728 332.8c-11.776-11.776-17.92-27.136-17.92-43.52s6.144-31.744 17.92-43.52L696.32 71.168c11.776-11.776 27.136-17.92 43.52-17.92s31.744 6.144 43.52 17.92L957.952 245.76c11.776 11.776 17.92 27.136 17.92 43.52s-6.144 31.744-17.92 43.52L783.36 507.392c-11.776 11.776-27.136 17.92-43.52 17.92z m0-411.136l-174.592 174.592 174.592 174.592 174.592-174.592-174.592-174.592zM376.832 960.512H130.048c-33.792 0-61.44-27.648-61.44-61.44v-247.296c0-33.792 27.648-61.44 61.44-61.44h247.296c33.792 0 61.44 27.648 61.44 61.44v247.296c-0.512 34.304-27.648 61.44-61.952 61.44z m-246.784-308.224v247.296h247.296v-247.296H130.048zM863.232 960.512h-247.296c-33.792 0-61.44-27.648-61.44-61.44v-247.296c0-33.792 27.648-61.44 61.44-61.44h247.296c33.792 0 61.44 27.648 61.44 61.44v247.296c0 34.304-27.136 61.44-61.44 61.44z m-246.784-308.224v247.296h247.296v-247.296h-247.296z" p-id="1168"></path></svg>',
+          // icon: '<svg t="1597727407471" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1167" width="32" height="32"><path d="M376.832 474.112H130.048c-33.792 0-61.44-27.648-61.44-61.44V165.888c0-33.792 27.648-61.44 61.44-61.44h247.296c33.792 0 61.44 27.648 61.44 61.44v247.296c-0.512 33.792-27.648 60.928-61.952 60.928zM130.048 165.888v247.296h247.296V165.888H130.048zM739.84 525.312c-16.384 0-31.744-6.144-43.52-17.92L521.728 332.8c-11.776-11.776-17.92-27.136-17.92-43.52s6.144-31.744 17.92-43.52L696.32 71.168c11.776-11.776 27.136-17.92 43.52-17.92s31.744 6.144 43.52 17.92L957.952 245.76c11.776 11.776 17.92 27.136 17.92 43.52s-6.144 31.744-17.92 43.52L783.36 507.392c-11.776 11.776-27.136 17.92-43.52 17.92z m0-411.136l-174.592 174.592 174.592 174.592 174.592-174.592-174.592-174.592zM376.832 960.512H130.048c-33.792 0-61.44-27.648-61.44-61.44v-247.296c0-33.792 27.648-61.44 61.44-61.44h247.296c33.792 0 61.44 27.648 61.44 61.44v247.296c-0.512 34.304-27.648 61.44-61.952 61.44z m-246.784-308.224v247.296h247.296v-247.296H130.048zM863.232 960.512h-247.296c-33.792 0-61.44-27.648-61.44-61.44v-247.296c0-33.792 27.648-61.44 61.44-61.44h247.296c33.792 0 61.44 27.648 61.44 61.44v247.296c0 34.304-27.136 61.44-61.44 61.44z m-246.784-308.224v247.296h247.296v-247.296h-247.296z" p-id="1168"></path></svg>',
+          icon: svgIcons.folder,
+          tipPosition: 's',
           toolbar: [
             {
+              hotkey: '⌘o',
               name: "openMdFile",
-              icon: '打开MD',
+              tipPosition: 's',
+              icon: '打开文件',
               click() {
                 self.openMdFile()
               }
             },
             {
+              hotkey: '⌘s',
               name: "exportMdFile",
-              icon: '导出MD',
+              icon: '导出...',
               click() {
                 self.saveMdFile()
-              }
-            },
-            {
-              name: "openProjectUrl",
-              icon: '项目主页',
-              click() {
-                self.openWindow(self.project_url)
               }
             },
           ],
         },
         "|",
-        "emoji",
-        "headings",
-        "bold",
-        "italic",
-        "strike",
-        "link",
+        {
+          hotkey: '⌘z',
+          name: 'undo',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '⌘y',
+          name: 'redo',
+          tipPosition: 's',
+          // tip: '恢复',
+        },
         "|",
-        "list",
-        "ordered-list",
-        "check",
-        "outdent",
-        "indent",
+        {
+          hotkey: '⌘e',
+          name: 'emoji',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '',
+          name: 'headings',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '⌘b',
+          name: 'bold',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '⌘i',
+          name: 'italic',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '',
+          name: 'strike',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '⌘k',
+          name: 'link',
+          tipPosition: 's',
+        },
         "|",
-        "quote",
-        "line",
-        "code",
-        "inline-code",
-        "insert-before",
-        "insert-after",
+        {
+          hotkey: '⌘l',
+          name: 'list',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '',
+          name: 'ordered-list',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '⌘j',
+          name: 'check',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '',
+          name: 'indent',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '',
+          name: 'outdent',
+          tipPosition: 's',
+        },
+        "|",
+        {
+          hotkey: '⌘;',
+          name: 'quote',
+          tipPosition: 's',
+          icon: svgIcons.quote
+        },
+        {
+          hotkey: '⌘-',
+          name: 'line',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '⌘u',
+          name: 'code',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '',
+          name: 'inline-code',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '⌘m',
+          name: 'table',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '',
+          name: 'insert-before',
+          tipPosition: 's',
+        },
+        {
+          hotkey: '',
+          name: 'insert-after',
+          tipPosition: 's',
+        },
         // "|",
         // "upload",
         // "record",
-        "table",
         "|",
-        "undo",
-        "redo",
-        "|",
-        // "fullscreen",
-        "edit-mode",
+        {
+          // hotkey: '',
+          name: 'edit-mode',
+          tipPosition: 's',
+        },
         {
           name: "more",
           toolbar: [
@@ -112,7 +196,7 @@ export default {
             "content-theme",
             "devtools",
             // "info",
-            // "help",
+            "help",
             {
               name: "about",
               icon: '关于',
