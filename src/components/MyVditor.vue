557a5c6567ebe7c141eef42640cc0d6776c8377c
jeeinn <xie19881108/>
2022-09-24 18:48:33 +0800

    F: 防止开发环境出现多个工具

--- src/components/MyVditor.vue
+++ src/components/MyVditor.vue
@@ -26,56 +26,58 @@ export default {
   },
   mounted() {
     let self = this
-    vditorConf.options.lange = this.lang // for i18n
-    vditorConf.options.placeholder = this.welcome
-    vditorConf.toolbar.unshift({
-      name: "openOrSave",
-      tip: "打开/保存",
-      // icon: '<svg t="1597727407471" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1167" width="32" height="32"><path d="M376.832 474.112H130.048c-33.792 0-61.44-27.648-61.44-61.44V165.888c0-33.792 27.648-61.44 61.44-61.44h247.296c33.792 0 61.44 27.648 61.44 61.44v247.296c-0.512 33.792-27.648 60.928-61.952 60.928zM130.048 165.888v247.296h247.296V165.888H130.048zM739.84 525.312c-16.384 0-31.744-6.144-43.52-17.92L521.728 332.8c-11.776-11.776-17.92-27.136-17.92-43.52s6.144-31.744 17.92-43.52L696.32 71.168c11.776-11.776 27.136-17.92 43.52-17.92s31.744 6.144 43.52 17.92L957.952 245.76c11.776 11.776 17.92 27.136 17.92 43.52s-6.144 31.744-17.92 43.52L783.36 507.392c-11.776 11.776-27.136 17.92-43.52 17.92z m0-411.136l-174.592 174.592 174.592 174.592 174.592-174.592-174.592-174.592zM376.832 960.512H130.048c-33.792 0-61.44-27.648-61.44-61.44v-247.296c0-33.792 27.648-61.44 61.44-61.44h247.296c33.792 0 61.44 27.648 61.44 61.44v247.296c-0.512 34.304-27.648 61.44-61.952 61.44z m-246.784-308.224v247.296h247.296v-247.296H130.048zM863.232 960.512h-247.296c-33.792 0-61.44-27.648-61.44-61.44v-247.296c0-33.792 27.648-61.44 61.44-61.44h247.296c33.792 0 61.44 27.648 61.44 61.44v247.296c0 34.304-27.136 61.44-61.44 61.44z m-246.784-308.224v247.296h247.296v-247.296h-247.296z" p-id="1168"></path></svg>',
-      icon: svgIcons.folder,
-      tipPosition: 's',
-      toolbar: [
-        {
-          hotkey: '⌘o',
-          name: "openMdFile",
-          tipPosition: 's',
-          icon: '打开文件',
-          click() {
-            self.openMdFile()
-          }
-        },
-        {
-          hotkey: '⌘s',
-          name: "exportMdFile",
-          icon: '导出...',
-          click() {
-            self.saveMdFile()
-          }
-        },
-      ],
-      click() {}
-    })
-    vditorConf.toolbar.push({
-      name: "more",
-      tipPosition: 's',
-      toolbar: [
-        // "export",
-        "outline",
-        "preview",
-        "both",
-        "code-theme",
-        "content-theme",
-        "devtools",
-        // "info",
-        "help",
-        {
-          name: "about",
-          icon: '关于',
-          click() {self.showAbout()}
-        },
-      ],
-    })
-    vditorConf.options.toolbar = vditorConf.toolbar;
+    if (!vditorConf.options.hasOwnProperty('lange')){
+      vditorConf.options.lange = this.lang // for i18n
+      vditorConf.options.placeholder = this.welcome
+      vditorConf.toolbar.unshift({
+        name: "openOrSave",
+        tip: "打开/保存",
+        // icon: '<svg t="1597727407471" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1167" width="32" height="32"><path d="M376.832 474.112H130.048c-33.792 0-61.44-27.648-61.44-61.44V165.888c0-33.792 27.648-61.44 61.44-61.44h247.296c33.792 0 61.44 27.648 61.44 61.44v247.296c-0.512 33.792-27.648 60.928-61.952 60.928zM130.048 165.888v247.296h247.296V165.888H130.048zM739.84 525.312c-16.384 0-31.744-6.144-43.52-17.92L521.728 332.8c-11.776-11.776-17.92-27.136-17.92-43.52s6.144-31.744 17.92-43.52L696.32 71.168c11.776-11.776 27.136-17.92 43.52-17.92s31.744 6.144 43.52 17.92L957.952 245.76c11.776 11.776 17.92 27.136 17.92 43.52s-6.144 31.744-17.92 43.52L783.36 507.392c-11.776 11.776-27.136 17.92-43.52 17.92z m0-411.136l-174.592 174.592 174.592 174.592 174.592-174.592-174.592-174.592zM376.832 960.512H130.048c-33.792 0-61.44-27.648-61.44-61.44v-247.296c0-33.792 27.648-61.44 61.44-61.44h247.296c33.792 0 61.44 27.648 61.44 61.44v247.296c-0.512 34.304-27.648 61.44-61.952 61.44z m-246.784-308.224v247.296h247.296v-247.296H130.048zM863.232 960.512h-247.296c-33.792 0-61.44-27.648-61.44-61.44v-247.296c0-33.792 27.648-61.44 61.44-61.44h247.296c33.792 0 61.44 27.648 61.44 61.44v247.296c0 34.304-27.136 61.44-61.44 61.44z m-246.784-308.224v247.296h247.296v-247.296h-247.296z" p-id="1168"></path></svg>',
+        icon: svgIcons.folder,
+        tipPosition: 's',
+        toolbar: [
+          {
+            hotkey: '⌘o',
+            name: "openMdFile",
+            tipPosition: 's',
+            icon: '打开文件',
+            click() {
+              self.openMdFile()
+            }
+          },
+          {
+            hotkey: '⌘s',
+            name: "exportMdFile",
+            icon: '导出...',
+            click() {
+              self.saveMdFile()
+            }
+          },
+        ],
+        click() {}
+      })
+      vditorConf.toolbar.push({
+        name: "more",
+        tipPosition: 's',
+        toolbar: [
+          // "export",
+          "outline",
+          "preview",
+          "both",
+          "code-theme",
+          "content-theme",
+          "devtools",
+          // "info",
+          "help",
+          {
+            name: "about",
+            icon: '关于',
+            click() {self.showAbout()}
+          },
+        ],
+      })
+      vditorConf.options.toolbar = vditorConf.toolbar;
+    }
     this.vditor = new Vditor('vditorEle', vditorConf.options)
   },
   methods: {
